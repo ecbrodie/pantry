17a4b84296204cf5c79d45c0706bd82d710dc0b1
Evan Brodie <brodie.evan@gmail.com>
2019-02-23 23:20:23 -0500

    Removes default app code; Replaces it with dummy native-base code

--- App.tsx
+++ App.tsx
@@ -1,21 +1,18 @@
 import React from "react"
-import { StyleSheet, Text, View } from "react-native"
+import { Container, Content, Header, Body, Title } from "native-base"
+import ShoppingList from "./src/ShoppingList"
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    )
-  }
+export default function App() {
+  return (
+    <Container>
+      <Header>
+        <Body>
+          <Title>Pantry</Title>
+        </Body>
+      </Header>
+      <Content>
+        <ShoppingList />
+      </Content>
+    </Container>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-})
