f2a7ffe45e83c401570a57c606012d75588c2517
Evan Brodie <brodie.evan@gmail.com>
2019-03-05 22:30:39 -0500

    Improves usage of setState() in the context; removes commented seed data

--- src/ShoppingListContext.tsx
+++ src/ShoppingListContext.tsx
@@ -15,18 +15,17 @@ type ShoppingListContextStore = {
 const ShoppingListContext = React.createContext({} as ShoppingListContextStore)
 export const Consumer = ShoppingListContext.Consumer
 
-// const seededData = ["apples", "bananas", "chocolate milk"].map(i => ({
-//   name: i,
-// }))
+type State = {
+  items: ShoppingListItem[]
+}
 
-export class Provider extends React.Component {
+export class Provider extends React.Component<{}, State> {
   state = {
     items: [],
-    // items: seededData,
   }
 
   addItem = (item: ShoppingListItem) => {
-    this.setState({ items: [...this.state.items, item] })
+    this.setState(({ items }) => ({ items: [...items, item] }))
   }
 
   render() {
