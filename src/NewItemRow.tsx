0fc9c37e1e2797b004cdbf111427499564ded3e1
Evan Brodie <brodie.evan@gmail.com>
2019-03-12 23:45:52 -0400

    Extracts NewItemRow into its own file

---
+++ src/NewItemRow.tsx
@@ -0,0 +1,55 @@
+import React from "react"
+import { AddItemFunc } from "./ShoppingListContext"
+import { ListItem, Item, Input, Icon, View } from "native-base"
+
+type Props = {
+  addItem: AddItemFunc
+}
+type State = {
+  newItemName: string
+}
+const DEFAULT_STATE = { newItemName: "" }
+
+export class NewItemRow extends React.PureComponent<Props, State> {
+  state = DEFAULT_STATE
+
+  submitItem = () => {
+    const { newItemName } = this.state
+    if (newItemName) {
+      this.props.addItem({ name: newItemName }, () =>
+        this.setState(() => DEFAULT_STATE),
+      )
+    }
+  }
+
+  render() {
+    return (
+      <ListItem>
+        <View style={{ flexDirection: "row-reverse", alignItems: "center" }}>
+          <View style={{ flex: 0, paddingLeft: 10 }}>
+            <Icon
+              name="ios-add-circle-outline"
+              style={{ color: "green" }}
+              onPress={this.submitItem}
+            />
+          </View>
+          <View style={{ flex: 1 }}>
+            <Item regular>
+              <Input
+                autoFocus
+                autoCapitalize="none"
+                blurOnSubmit={false}
+                placeholder="New Item"
+                value={this.state.newItemName}
+                onChangeText={value =>
+                  this.setState(() => ({ newItemName: value }))
+                }
+                onSubmitEditing={this.submitItem}
+              />
+            </Item>
+          </View>
+        </View>
+      </ListItem>
+    )
+  }
+}
--- src/ShoppingList.tsx
+++ src/ShoppingList.tsx
@@ -1,6 +1,7 @@
 import React from "react"
+import { Text, List, ListItem } from "native-base"
 import { ShoppingListItem, AddItemFunc } from "./ShoppingListContext"
-import { Text, List, ListItem, Item, Input, Icon, View } from "native-base"
+import { NewItemRow } from "./NewItemRow"
 
 type Props = {
   items: ShoppingListItem[]
@@ -13,6 +14,7 @@ export default function ShoppingList({
   showNewItemRow = false,
 }: Props) {
   if (items.length === 0 && !showNewItemRow) return <Text>No Items</Text>
+
   return (
     <List>
       {items.map(({ name }) => (
@@ -23,56 +25,4 @@ export default function ShoppingList({
       {showNewItemRow && <NewItemRow addItem={addItem} />}
     </List>
   )
-}
-
-type NewItemRowProps = {
-  addItem: AddItemFunc
-}
-type NewItemRowState = {
-  newItemName: string
-}
-const DEFAULT_STATE = { newItemName: "" }
-class NewItemRow extends React.PureComponent<NewItemRowProps, NewItemRowState> {
-  state = DEFAULT_STATE
-
-  submitItem = () => {
-    const { newItemName } = this.state
-
-    if (newItemName) {
-      this.props.addItem({ name: newItemName }, () =>
-        this.setState(() => DEFAULT_STATE),
-      )
-    }
-  }
-
-  render() {
-    return (
-      <ListItem>
-        <View style={{ flexDirection: "row-reverse", alignItems: "center" }}>
-          <View style={{ flex: 0, paddingLeft: 10 }}>
-            <Icon
-              name="ios-add-circle-outline"
-              style={{ color: "green" }}
-              onPress={this.submitItem}
-            />
-          </View>
-          <View style={{ flex: 1 }}>
-            <Item regular>
-              <Input
-                autoFocus
-                autoCapitalize="none"
-                blurOnSubmit={false}
-                placeholder="New Item"
-                value={this.state.newItemName}
-                onChangeText={value =>
-                  this.setState(() => ({ newItemName: value }))
-                }
-                onSubmitEditing={this.submitItem}
-              />
-            </Item>
-          </View>
-        </View>
-      </ListItem>
-    )
-  }
 }
