7a0ce0b84ee7ba2f7367bbf5296792e20d550735
Evan Brodie <brodie.evan@gmail.com>
2019-03-17 12:35:58 -0400

    Adds UI for row deletion button

--- src/ItemRow.tsx
+++ src/ItemRow.tsx
@@ -1,13 +1,25 @@
 import React from "react"
-import { ListItem, Text } from "native-base"
+import { ListItem, Text, View, Icon } from "native-base"
 
 interface Props {
   name: string
 }
+
 export default function ItemRow({ name }: Props) {
   return (
     <ListItem>
-      <Text>{name}</Text>
+      <View style={{ flexDirection: "row", alignItems: "center" }}>
+        <View style={{ flex: 1 }}>
+          <Text>{name}</Text>
+        </View>
+        <View style={{ flex: 0 }}>
+          <Icon
+            name="md-close-circle-outline"
+            onPress={() => console.log("PRESSED")}
+            style={{ color: "dimgrey" }}
+          />
+        </View>
+      </View>
     </ListItem>
   )
 }
