b620104ed291f962beee60165346751f81ba2c8a
Evan Brodie <brodie.evan@gmail.com>
2020-05-01 00:08:46 -0400

    Converts some react class components into function + state hooks

--- src/NewItemRow.tsx
+++ src/NewItemRow.tsx
@@ -1,55 +1,42 @@
-import React from "react"
+import React, { useState } from "react"
 import { AddItemFunc } from "./ShoppingListContext"
 import { ListItem, Item, Input, Icon, View } from "native-base"
 
 interface Props {
   addItem: AddItemFunc
 }
-interface State {
-  newItemName: string
-}
-const DEFAULT_STATE = { newItemName: "" }
-
-export default class NewItemRow extends React.PureComponent<Props, State> {
-  state = DEFAULT_STATE
-
-  submitItem = () => {
-    const { newItemName } = this.state
+export default function NewItemRow({ addItem }: Props) {
+  const [newItemName, setNewItemName] = useState("")
+  const submitItem = () => {
     if (newItemName) {
-      this.props.addItem({ name: newItemName }, () =>
-        this.setState(() => DEFAULT_STATE),
-      )
+      addItem({ name: newItemName }, () => setNewItemName(""))
     }
   }
 
-  render() {
-    return (
-      <ListItem>
-        <View style={{ flexDirection: "row-reverse", alignItems: "center" }}>
-          <View style={{ flex: 0, paddingLeft: 10 }}>
-            <Icon
-              name="ios-add-circle-outline"
-              style={{ color: "green" }}
-              onPress={this.submitItem}
-            />
-          </View>
-          <View style={{ flex: 1 }}>
-            <Item regular>
-              <Input
-                autoFocus
-                autoCapitalize="none"
-                blurOnSubmit={false}
-                placeholder="New Item"
-                value={this.state.newItemName}
-                onChangeText={value =>
-                  this.setState(() => ({ newItemName: value }))
-                }
-                onSubmitEditing={this.submitItem}
-              />
-            </Item>
-          </View>
+  return (
+    <ListItem>
+      <View style={{ flexDirection: "row-reverse", alignItems: "center" }}>
+        <View style={{ flex: 0, paddingLeft: 10 }}>
+          <Icon
+            name="ios-add-circle-outline"
+            style={{ color: "green" }}
+            onPress={submitItem}
+          />
         </View>
-      </ListItem>
-    )
-  }
+        <View style={{ flex: 1 }}>
+          <Item regular>
+            <Input
+              autoFocus
+              autoCapitalize="none"
+              blurOnSubmit={false}
+              placeholder="New Item"
+              value={newItemName}
+              onChangeText={setNewItemName}
+              onSubmitEditing={submitItem}
+            />
+          </Item>
+        </View>
+      </View>
+    </ListItem>
+  )
 }
--- src/ShoppingListPage.tsx
+++ src/ShoppingListPage.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import {
   Fab,
   Icon,
@@ -16,60 +16,47 @@ import ShoppingList from "./ShoppingList"
 
 const defaultIconBlue = "#5065F6"
 
-interface State {
-  addingItem: boolean
-}
+export default function ShoppingListPage() {
+  const [addingItem, setAddingItem] = useState(false)
+  const fabIconName = addingItem ? "md-checkmark-circle" : "md-add-circle"
+  const fabIconColor = addingItem ? "green" : defaultIconBlue
 
-export default class ShoppingListPage extends React.Component<{}, State> {
-  state = { addingItem: false }
-
-  render() {
-    const fabIconName = this.state.addingItem
-      ? "md-checkmark-circle"
-      : "md-add-circle"
-    const fabIconColor = this.state.addingItem ? "green" : defaultIconBlue
-
-    return (
-      <Consumer>
-        {({ items, addItem, removeItem, removeAllItems }) => {
-          return (
-            <Container>
-              <Header>
-                <Body>
-                  <Title>Shopping List</Title>
-                </Body>
-                <Right>
-                  <Button
-                    transparent
-                    disabled={items.length === 0}
-                    onPress={removeAllItems}
-                  >
-                    <Text>Clear All</Text>
-                  </Button>
-                </Right>
-              </Header>
-              <Content enableOnAndroid padder>
-                <ShoppingList
-                  addItem={addItem}
-                  items={items}
-                  removeItem={removeItem}
-                  showNewItemRow={this.state.addingItem}
-                />
-              </Content>
-              <Fab
-                onPress={() =>
-                  this.setState(({ addingItem }) => ({
-                    addingItem: !addingItem,
-                  }))
-                }
-                style={{ backgroundColor: fabIconColor }}
-              >
-                <Icon name={fabIconName} />
-              </Fab>
-            </Container>
-          )
-        }}
-      </Consumer>
-    )
-  }
+  return (
+    <Consumer>
+      {({ items, addItem, removeItem, removeAllItems }) => {
+        return (
+          <Container>
+            <Header>
+              <Body>
+                <Title>Shopping List</Title>
+              </Body>
+              <Right>
+                <Button
+                  transparent
+                  disabled={items.length === 0}
+                  onPress={removeAllItems}
+                >
+                  <Text>Clear All</Text>
+                </Button>
+              </Right>
+            </Header>
+            <Content enableOnAndroid padder>
+              <ShoppingList
+                addItem={addItem}
+                items={items}
+                removeItem={removeItem}
+                showNewItemRow={addingItem}
+              />
+            </Content>
+            <Fab
+              onPress={() => setAddingItem(!addingItem)}
+              style={{ backgroundColor: fabIconColor }}
+            >
+              <Icon name={fabIconName} />
+            </Fab>
+          </Container>
+        )
+      }}
+    </Consumer>
+  )
 }
