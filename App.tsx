61d97faf11f896741104d7574c9518e78cd4816d
Evan Brodie <brodie.evan@gmail.com>
2020-05-02 23:33:05 -0400

    Rewrites App component with hooks

--- App.tsx
+++ App.tsx
@@ -1,38 +1,35 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Ionicons } from "@expo/vector-icons"
-import * as Font from 'expo-font';
+import * as Font from "expo-font"
 import ShoppingListPage from "./src/ShoppingListPage"
 import { Provider } from "./src/ShoppingListContext"
 import { Root } from "native-base"
 import "./src/firebase"
 
-export default class App extends React.Component {
-  state = {
-    fontLoaded: false,
-  }
-
-  async componentDidMount() {
-    // Workaround for case-sensitivity issues
-    // Expo sets as lowercase, but native-base expects PascalCase
-    await Font.loadAsync({
-      Ionicons: Ionicons.font.ionicons,
-      Roboto: require("native-base/Fonts/Roboto.ttf"),
-      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
-    })
-
-    this.setState({ fontLoaded: true })
-  }
-
-  render() {
-    if (!this.state.fontLoaded) {
-      return null
+export default function() {
+  const [fontLoaded, setFontLoaded] = useState(false)
+  useEffect(() => {
+    const loadFonts = async () => {
+      // Workaround for case-sensitivity issues
+      // Expo sets as lowercase, but native-base expects PascalCase
+      await Font.loadAsync({
+        Ionicons: Ionicons.font.ionicons,
+        Roboto: require("native-base/Fonts/Roboto.ttf"),
+        Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+      })
     }
-    return (
-      <Root>
-        <Provider>
-          <ShoppingListPage />
-        </Provider>
-      </Root>
-    )
+    loadFonts()
+    setFontLoaded(true)
+  }, [])
+
+  if (!fontLoaded) {
+    return null
   }
+  return (
+    <Root>
+      <Provider>
+        <ShoppingListPage />
+      </Provider>
+    </Root>
+  )
 }
